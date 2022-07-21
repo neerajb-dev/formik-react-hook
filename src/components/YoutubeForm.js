d3026f33f8e0c9a24618bc335d9726d7e28c4c3a
Neeraj Bhardwaj <61327327+neerajb-dev@users.noreply.github.com>
2022-07-22 03:50:31 +0530

    Refactoring code using Formik Components Formik, Form, Fields, ErrorMessage are all Formik Components which replaces traditional html form tags

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FormikContext, useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
@@ -15,68 +15,49 @@ const onSubmit = (values) => {
 // yup validation schema object
 const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
-  email: Yup.string().email("Invalid email format").required("required"),
+  email: Yup.string().email("Invalid email format").required("Required"),
   channel: Yup.string().required("Required")
 });
 
 const YoutubeForm = () => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema
-    // validate
-  });
-
   // console.log("Form values", formik.values);
   // console.log("form errors", formik.errors);
-  console.log("Visited fields", formik.touched);
+  // console.log("Visited fields", formik.touched);
 
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...formik.getFieldProps("name")}
-          />
+          <Field type="text" id="name" name="name" className="input-field" />
           {/* render error if name is empty */}
-          {formik.touched.name && formik.errors.name ? (
-            <div className="error">{formik.errors.name}</div>
-          ) : null}
+          <ErrorMessage name="name" className="error" />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            {...formik.getFieldProps("email")}
-          />
+          <Field type="email" id="email" name="email" className="input-field" />
           {/* render errors if email is empty or invalid */}
-          {formik.touched.email && formik.errors.email ? (
-            <div className="error">{formik.errors.email}</div>
-          ) : null}
+          <ErrorMessage name="email" className="error" />
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input
+          <Field
             type="text"
             id="channel"
             name="channel"
-            {...formik.getFieldProps("channel")}
+            className="input-field"
           />
           {/* render errors if email is empty or invalid */}
-          {formik.touched.channel && formik.errors.channel ? (
-            <div className="error">{formik.errors.channel}</div>
-          ) : null}
+          <ErrorMessage name="channel" className="error" />
         </div>
         <button type="submit">Submit</button>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   );
 };
 
